b4f94bb7ec3914a46791016d0a9a7ece0e8f7dcf
lokeshneerati <lokeshneerati84@gmail.com>
2024-06-22 15:47:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function fibonacci(num) {
 // your code here
+	// Base Case
+	if(num === 0 || num === 1){
+		return num;
+
+	}
+	// Recursive case 
+	fibonacci(num - 1) + fibonacci(num - 2);
 }
 
 module.exports = fibonacci;
